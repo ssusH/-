bea03269e2455fda4c2907fd54ac17bf37d731fd
ssusH <781245889@qq.com>
2017-03-09 11:54:10 +0800

    2017-03-09 11:50:58
    上传工程文件

---
+++ Assets/Test/Shot/bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour
+{
+
+    public float power;
+    public string type;
+    public float speed;
+
+    void OnCollisionEnter(Collision other)
+    {
+            Destroy(this.gameObject);
+        if (other.transform.tag == "Enemy")
+        {
+            //Destroy(other.gameObject);
+        }
+
+    }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if(other.transform.tag == "Player")
+    //    {
+    //        this.gameObject.GetComponent<SphereCollider>().isTrigger = false;
+    //    }
+    //}
+
+
+}
